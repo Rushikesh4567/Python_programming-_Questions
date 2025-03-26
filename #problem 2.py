93a1e5ffeea0ddb6d546c545212ecd42511eca8e
Rushikesh Shivanand Anantpure <rushikeksh4567@gmail.com>
2025-03-26 10:56:48 +0530

    Add files via upload

---
+++ #problem 2.py
@@ -0,0 +1,8 @@
+#problem 2 streetlight lamps
+
+number=int(input("Enter street light number "))
+
+if number%2==0:
+    print("Street light ",number,"is ON")
+else:
+    print("Street light ",number,"is OFF")
