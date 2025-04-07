b88fb51978ccbfea09542fc64a027aa71006f45e
Rushikesh Shivanand Anantpure <rushikeksh4567@gmail.com>
2025-04-07 08:11:19 -0700

    Add files via upload

---
+++ #problem 15.py
@@ -0,0 +1,14 @@
+#problem 15
+def max_num(a,b,c):
+    if a>b and a>c:
+        return a
+    elif b>a and b>c:
+        return b
+    else:
+        return c
+
+a=int(input("Enter 1st number= "))
+b=int(input("Enter 2nd number= "))
+c=int(input("Enter 3rd number= "))
+
+print("\nThe maximum of three is ",max_num(a,b,c))
